e8cf7ec5e48eb9433ef4579e7eca053a819f18b3
student <student@Alpha3>
2018-11-26 09:56:49 +0300

    First commit

---
+++ src/Testing/DoCalculationTestThree.java
@@ -0,0 +1,17 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Program.FrameUI;
+
+public class DoCalculationTestThree {
+
+	@Test
+	public void testCalculation() {
+		FrameUI frame = new FrameUI("Страховой калькулятор", 820, 780); /*Создани экземпляра класса FrameUI*/
+		assertEquals("22714.0" ,frame.Calculation(2965.0, 2.0, 1.4, 1.8 ,1.0, 1.6, 0.95,1.0,1.0));  /*Проверка соответствия ожидаемого и реального результата*/
+	}
+
+}
