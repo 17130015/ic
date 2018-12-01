842b55f08e26c673c75e2e373ed116de42df1a90
17130015 <44563365+17130015@users.noreply.github.com>
2018-12-01 10:11:03 +0300

    Update DoCalculationTestThree1.java

--- src/Testing/DoCalculationTestThree1.java
+++ src/Testing/DoCalculationTestThree1.java
@@ -2,7 +2,7 @@ package Testing;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import Program.FrameUI;
-public class DoCalculationTestThree {
+public class DoCalculationTestThree1 {
 	@Test
 	public void testCalculation() {
 	FrameUI frame = new FrameUI("Страховой калькулятор", 820, 780); /*Создани экземпляра класса FrameUI*/
