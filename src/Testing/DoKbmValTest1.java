f18e656268c487336a6a3bc00434d995fce9633c
17130015 <44563365+17130015@users.noreply.github.com>
2018-12-01 10:07:53 +0300

    Update DoKbmValTest1.java

--- src/Testing/DoKbmValTest1.java
+++ src/Testing/DoKbmValTest1.java
@@ -1,32 +1,26 @@
 package Testing;
-
 import static org.junit.Assert.*;
-
-
 import org.junit.Test;
-
 import Program.FrameUI;
-
-public class DoKbmValTest1 {
-
+public class DoKbmValTest {
 	@Test
 	public void testKbmVal() {
-		FrameUI frame = new FrameUI("��������� �����������", 820, 780); /*������� ���������� ������ FrameUI*/
-		assertEquals(Double.toString(frame.KbmVal(0)),"2.45"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(1)),"2.3"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(2)),"1.55"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(3)),"1.4"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(4)),"1.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(5)),"0.95"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(6)),"0.9"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(7)),"0.85"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(8)),"0.8"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(9)),"0.75"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(10)),"0.7"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(11)),"0.65"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(12)),"0.6"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(13)),"0.55"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.KbmVal(14)),"0.5"); /*�������� ������������ ���������� � ��������� ����������*/
+		FrameUI frame = new FrameUI("Страховой калькулятор", 820, 780); /*Создани экземпляра класса FrameUI*/
+		assertEquals(Double.toString(frame.KbmVal(0)),"2.45"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(1)),"2.3"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(2)),"1.55"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(3)),"1.4"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(4)),"1.0"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(5)),"0.95"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(6)),"0.9"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(7)),"0.85"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(8)),"0.8"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(9)),"0.75"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(10)),"0.7"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(11)),"0.65"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(12)),"0.6"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(13)),"0.55"); /*Проверка соответствия ожидаемого и реального результата*/
+		assertEquals(Double.toString(frame.KbmVal(14)),"0.5"); /*Проверка соответствия ожидаемого и реального результата*/
 	}
 
 }
